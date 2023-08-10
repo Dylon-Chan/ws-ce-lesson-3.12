82d20cc56ba2e2ed8c3c372cb0de89e95a2026de
Dylon-Chan <dylon_chan22@hotmail.com>
2023-08-11 00:37:45 +0800

    upload test

---
+++ .github/tests/test.js
@@ -0,0 +1,12 @@
+const { handler } = require('./index');
+
+test('Handler returns the correct response', async () => {
+  
+  const result = await handler(event);
+
+  expect(result.statusCode).toBe(200);
+  expect(JSON.parse(result.body)).toEqual({
+    message: "Go Serverless v3.0! Your function executed successfully!",
+    input: event,
+  });
+});
